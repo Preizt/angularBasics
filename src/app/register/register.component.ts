ecdb2b1aaa9deadcb9b20cb5f7c25c57a282802b
Preizt <prieztly.tennysson@gmail.com>
2025-04-24 12:52:01 +0530

    second commit

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  //properties
+  firstName: string = '';
+  LastName: string = '';
 
+  register() {
+    alert(this.firstName);
+  }
+
+  onFirstNameChange(fname: any) {
+    console.log(fname.target.value);
+    this.firstName = fname.target.value;
+  }
+
+  lastNameClick(lastName: any) {
+    this.LastName = lastName.target.value;
+    // console.log(lastName.value);
+
+    alert(lastName.value);
+  }
 }
